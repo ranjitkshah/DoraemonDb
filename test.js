a8e80a726e7d6d2c03ea1d43e88faa565da4b1ed
ranjit <ranjitmaheshshah@gmail.com>
2020-12-31 22:20:01 +0530

    implemented in better way

--- test.js
+++ test.js
@@ -1,11 +1,20 @@
 const { Console } = require('console');
 const doraemonDb = require('./src/index');
 
-doraemonDb('1','ranjit',5)
-// console.log(SData('1'))
-console.log(doraemonDb.all())
-// console.log(SData.read("1"))
-setTimeout(()=>{
-    console.log(doraemonDb.read('1'))
-},2000)
+// create db json || args( key: string, value: string, TTL: number )
+// ttl (time to leave) for key expiration in seconds
+doraemonDb('1','ranjit',5) 
+
+// read db || args (key: string) returns value of key
+doraemonDb.read('1')
+
+// has db || args (key: string) returns boolean
+doraemonDb.has('1')
+
+// all db || returns all data storage
+doraemonDb.all()
+
+//delete db ||  args (key: string) delete value of key
+doraemonDb.delete('1')
+
 
